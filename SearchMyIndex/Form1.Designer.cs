d7f61df338408a96da5d9096c112e04e733c718d
Masao Yoshioka <mayo@aqua.biglobe.ne.jp>
2020-11-25 16:36:08 +0900

    プロジェクト ファイルを追加します。

---
+++ SearchMyIndex/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace SearchMyIndex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSearchText = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lstSearchResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtSearchText
+            // 
+            this.txtSearchText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSearchText.Location = new System.Drawing.Point(13, 13);
+            this.txtSearchText.Name = "txtSearchText";
+            this.txtSearchText.Size = new System.Drawing.Size(808, 19);
+            this.txtSearchText.TabIndex = 0;
+            this.txtSearchText.Enter += new System.EventHandler(this.txtSearchText_Enter);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSearch.Location = new System.Drawing.Point(835, 11);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(67, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "検索";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lstSearchResult
+            // 
+            this.lstSearchResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstSearchResult.FormattingEnabled = true;
+            this.lstSearchResult.ItemHeight = 12;
+            this.lstSearchResult.Location = new System.Drawing.Point(13, 50);
+            this.lstSearchResult.Name = "lstSearchResult";
+            this.lstSearchResult.Size = new System.Drawing.Size(889, 340);
+            this.lstSearchResult.TabIndex = 2;
+            this.lstSearchResult.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lstSearchResult_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 406);
+            this.Controls.Add(this.lstSearchResult);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearchText);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "SearchMyIndex";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearchText;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListBox lstSearchResult;
+    }
+}
+
---
+++ SearchMyIndex/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SearchMyIndex
+{
+    public partial class Form1 : Form
+    {
+        private const string CONFKEY_COMPUTERNAME = "computername";
+        private const string CONFKEY_DIR = "dir";
+        private const string CONFKEY_SHARE_DIR = "shareDir";
+        private const string CONFKEY_BACLUP_DIR = "backupDir";
+        private const string CONFKEY_BACLUP_SHARE_DIR = "backupShareDir";
+
+        private List<Dictionary<string, string>> confList = new List<Dictionary<string, string>>();
+        private List<string> fullNameList = new List<string>();
+
+        private Dictionary<string, Dictionary<string, string>> resultConfDictionary = new Dictionary<string, Dictionary<string, string>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LoadMyIndexes();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Location = Properties.Settings.Default.Form1Location;
+            Size = Properties.Settings.Default.Form1Size;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Form1Location = Location;
+            Properties.Settings.Default.Form1Size = Size;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                if (ActiveControl == lstSearchResult && lstSearchResult.SelectedIndex <= 0)
+                {
+                    txtSearchText.Focus();
+                }
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                if (ActiveControl == txtSearchText)
+                {
+                    lstSearchResult.Focus();
+                    if (lstSearchResult.Items.Count > 0)
+                    {
+                        lstSearchResult.SelectedIndex = 0;
+                    }
+                }
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                if (ActiveControl == txtSearchText)
+                {
+                    btnSearch.PerformClick();
+                }
+
+                if (ActiveControl == lstSearchResult)
+                {
+                    ExecItem();
+                }
+            }
+        }
+
+        private void txtSearchText_Enter(object sender, EventArgs e)
+        {
+            txtSearchText.SelectAll();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            SearchIndex(txtSearchText.Text);
+        }
+
+        private void lstSearchResult_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ExecItem();
+        }
+
+        private void LoadMyIndexes()
+        {
+            string[] indexes = Directory.GetFiles(Properties.Settings.Default.MyIndexDir);
+
+            foreach (string index in indexes)
+            {
+                LoadMyIndex(index);
+            }
+        }
+
+        private void LoadMyIndex(string index)
+        {
+            Dictionary<string, string> conf = new Dictionary<string, string>();
+
+            using (StreamReader sr = new StreamReader(index, Encoding.GetEncoding("shift_jis")))
+            {
+                while (sr.Peek() > -1)
+                {
+                    string lineStr = sr.ReadLine();
+
+                    if (lineStr == "")
+                    {
+                        break;
+                    }
+
+                    string[] keyValue = lineStr.Split('=');
+                    if (keyValue.Length != 2)
+                    {
+                        break;
+                    }
+
+                    string key = keyValue[0];
+                    string value = keyValue[1];
+                    conf[key] = value;
+                }
+
+                if (String.Compare(conf[CONFKEY_COMPUTERNAME], Environment.MachineName, true) == 0)
+                {
+                    // インデックスと同じPC
+
+                    string dir = conf[CONFKEY_DIR];
+
+                    while (sr.Peek() > -1)
+                    {
+                        string lineStr = sr.ReadLine();
+
+                        if (lineStr != "")
+                        {
+                            confList.Add(conf);
+
+                            string fullName = Path.Combine(dir, lineStr);
+                            fullNameList.Add(fullName);
+                        }
+                    }
+                }
+                else
+                {
+                    // インデックスと異なるPC
+
+                    string shareDir = conf[CONFKEY_SHARE_DIR];
+
+                    while (sr.Peek() > -1)
+                    {
+                        string lineStr = sr.ReadLine();
+
+                        if (lineStr != "")
+                        {
+                            confList.Add(conf);
+
+                            string fullName = Path.Combine(shareDir, lineStr);
+                            fullNameList.Add(fullName);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void SearchIndex(string searchStr)
+        {
+            lstSearchResult.Items.Clear();
+            resultConfDictionary.Clear();
+
+            List<string> searchResultList = new List<string>();
+
+            for (int i = 0; i < fullNameList.Count; i++)
+            {
+                string fullName = fullNameList[i];
+
+                if (CultureInfo.CurrentCulture.CompareInfo.IndexOf(fullName, searchStr, CompareOptions.IgnoreCase | CompareOptions.IgnoreWidth | CompareOptions.IgnoreKanaType) >= 0)
+                {
+                    searchResultList.Add(fullName);
+
+                    Dictionary<string, string> conf = confList[i];
+                    resultConfDictionary.Add(fullName, conf);
+                }
+            }
+
+            searchResultList.Sort();
+
+            lstSearchResult.BeginUpdate();
+
+            for (int i = 0; i < searchResultList.Count; i++)
+            {
+                lstSearchResult.Items.Add(searchResultList[i]);
+            }
+
+            lstSearchResult.EndUpdate();
+        }
+
+        private void ExecItem()
+        {
+            string fullName = (string)lstSearchResult.SelectedItem;
+
+            if (Directory.Exists(fullName))
+            {
+                Dictionary<string, string> conf = resultConfDictionary[fullName];
+                if (conf.ContainsKey(CONFKEY_BACLUP_DIR))
+                {
+                    string shareDir = conf[CONFKEY_SHARE_DIR];
+                    string backupShareDir = conf[CONFKEY_BACLUP_SHARE_DIR];
+
+                    string normalizationFullName = Path.GetFullPath(fullName);
+                    string partName = null;
+                    string backupFullName = null;
+                    if (String.Compare(conf[CONFKEY_COMPUTERNAME], Environment.MachineName, true) == 0 || shareDir == null || backupShareDir == null)
+                    {
+                        string dir = conf[CONFKEY_DIR];
+                        string backupDir = conf[CONFKEY_BACLUP_DIR];
+
+                        partName = normalizationFullName.Substring(dir.Length + 1);
+                        backupFullName = Path.Combine(backupDir, partName);
+                    }
+                    else
+                    {
+                        partName = normalizationFullName.Substring(shareDir.Length + 1);
+                        backupFullName = Path.Combine(backupShareDir, partName);
+                    }
+
+                    if (Directory.Exists(backupFullName))
+                    {
+                        //Process.Start("explorer.exe", @"/select,""" + backupFullName + @"""");
+                        Process.Start(backupFullName);
+                    }
+                }
+
+                //Process.Start("explorer.exe", @"/select,""" + fullName + @"""");
+                Process.Start(fullName);
+            }
+            else if (File.Exists(fullName))
+            {
+                Process.Start(fullName);
+            }
+        }
+    }
+}
